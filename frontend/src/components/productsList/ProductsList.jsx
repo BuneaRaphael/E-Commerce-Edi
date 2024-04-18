b03d8aef36a2016658b9625bf1b587cb118bf78f
Bunea Raphael <bunea.v.raphael@gmail.com>
2024-04-18 21:49:24 +0300

    Ia ma

--- frontend/src/components/productsList/ProductsList.jsx
+++ frontend/src/components/productsList/ProductsList.jsx
@@ -11,7 +11,11 @@ const ProductList = () => {
     const fetchData = async () => {
       try {
         const response = await fetchProducts();
-        setProducts(response.data);
+        if (!response) {
+          throw new Error("Failed to fetch products");
+        }
+        setProducts(response);
+
         setIsLoading(false); // Data fetched, set loading state to false
       } catch (error) {
         console.error("Error fetching products:", error);
@@ -21,13 +25,13 @@ const ProductList = () => {
 
     fetchData();
   }, []);
-
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
   return (
     <div>
       <h2>Products</h2>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : products.length > 0 ? (
+      {products.length > 0 ? (
         products.map((product) => (
           <Product key={product._id} product={product} />
         ))
